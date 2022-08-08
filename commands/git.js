63873ebecbf3efe0bdb7ef3c4b702be64ae22245
Glitch (berry-reflective-slime) <none>
2022-08-08 13:13:41 +0000

    🚌📷 Checkpoint ./commands/git.js:47468821/2519 ./commands.js:47468821/758

--- commands/git.js
+++ commands/git.js
@@ -1,22 +1,27 @@
-import {
-  InteractionResponseType
-} from "discord-interactions";
+import { InteractionResponseType } from "discord-interactions";
 
-function gitHandler(res,req,data){
-  console.log(data)
+function gitHandler(res, req, data) {
+  console.log(data);
   res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: "GIT you have send msg: "
-      },
-    })
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+      content: "GIT you have send msg: ",
+    },
+  });
 }
 
 // Simple git command
 export const GIT_COMMAND = {
   name: "git",
-  description: 'Git command Q&A bot',
+  description: "Git command Q&A bot",
   type: 1,
-  option: 
-  handler: gitHandler
+  options: [
+    {
+      name: "question",
+      description: "Question user want to ask",
+      type: 3,
+      required: true,
+    },
+  ],
+  handler: gitHandler,
 };
