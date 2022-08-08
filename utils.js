ced563224ba9ad9d81c491bb1e3896863c457794
Glitch (berry-reflective-slime) <none>
2022-08-08 11:29:31 +0000

    🐴🏫 Checkpoint ./app.js:47468821/46 ./utils.js:47468821/207

--- utils.js
+++ utils.js
@@ -9,11 +9,11 @@ export function VerifyDiscordRequest(clientKey) {
 
     const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
     if (!isValidRequest) {
-      console.log(clientKey)
-      console.log(signature)
       res.status(401).send('Bad request signature');
       throw new Error('Bad request signature');
     }
+    
+    console.log("url:"+req.originalUrl)
   };
 }
 
