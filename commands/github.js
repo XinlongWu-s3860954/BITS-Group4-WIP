27a283d73c7a5a3bc0db7a08e5101d972b4b7df3
Glitch (berry-reflective-slime) <none>
2022-09-07 00:37:28 +0000

    ⛴🙍 Checkpoint ./commands/github.js:47468821/717

--- commands/github.js
+++ commands/github.js
@@ -1,27 +1,37 @@
 import { InteractionResponseType } from "discord-interactions";
+import rake from "node-rake"
 import fs from 'fs';
-import { parse } from 'yaml'
+import { parse, stringify } from 'yaml'
 
 var github_data = parse(fs.readFileSync('./data/github.yaml', 'utf8'))
+
+function findAnswer(key_words){
+  let ans = {};
+  let max_hit_count = 0;
+  
+  github_data.forEach((element) => {
+      let hit_count = 0;
+      key
+  });
+}
 
 function githubHandler(res, req, data) {
   if (!data.options) {
     res.send({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
-        content: "c",
+        content: "Sorry, Did you say anything?",
       },
     });
     return
   }
 
-  // console.log(data);
   let msg = data.options[0].value;
-  // console.log(msg)
+  let key_words = rake.generate(msg);
   res.send({
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: {
-      content: "GITHUB you have send msg: " + msg,
+      content: "key_words: " + key_words,
     },
   });
 }
